c69ddb40ae05b847d428b1683e0e24e33b2c9acc
Khachatur Gevorgyan <khachatur.gevorgyan.1993@gmail.com>
2021-07-19 20:30:22 +0400

    Adding an interface for game

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+  title = 'Mortal Kombat';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+// LOGIC IN PROGRESS
